75303928d4cbf6d968b19234ec486ff968645794
rusli abdul gani <rusli.gani88@gmail.com>
2017-07-03 06:49:56 +0700

    sudoku2 done

--- sudoku.js
+++ sudoku.js
@@ -1,24 +1,132 @@
 "use strict"
+const fs = require('fs');
+const board_string = fs.readFileSync('set-01_sample.unsolved.txt').toString().split("\n")[0]
 
 class Sudoku {
-  constructor(board_string) {}
+  constructor(board_string) {
+    this._board = this.makeBoard();
+  }
 
-  solve() {}
+  makeBoard(){
+    let tempArray1 = [];
+    for(let i=0 ; i<board_string.length ; i++){
+      tempArray1.push(Number(board_string[i]));
+    }
+    let tempArray2 = [];
+    for(let i=0 ; i<81 ; i+=9){
+      tempArray2.push(tempArray1.slice(i,i+9));
+    }
+    return tempArray2;
+  }
 
-  // Returns a string representing the current state of the board
-  board() {}
+  get board(){
+    return this._board;
+  }
+
+  checkEmptyPosition(){
+    let array = [];
+    for(let a=0 ; a<this.board.length ; a++){
+      for(let b=0 ; b<this.board[a].length ; b++){
+        if(this.board[a][b]===0){
+          array.push([a,b]);
+        }
+      }
+    }
+    return array;
+  }
+
+  checkRow(row,value){
+    for(let a=0 ; a<9 ; a++){
+      if(this.board[row][a]==value){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  checkColumn(col,value){
+    for(let a=0 ; a<9 ; a++){
+      if(this.board[a][col]==value){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  checkSquare(row, col, value){
+    let baseRow = 0;
+    let baseColumn = 0;
+    let dimension = 3;
+    while (row >= baseRow + dimension) {
+      baseRow += dimension;
+    }
+    while(col >= baseColumn + dimension){
+      baseColumn += dimension;
+    }
+    for(let a=baseRow ; a<baseRow+dimension ; a++){
+      for(let b=baseColumn ; b<baseColumn+dimension ; b++){
+        if(this.board[baseRow][baseColumn]===value){
+          return false;
+        } else{
+          return true;
+        }
+      }
+    }
+  }
+
+  // solve() {
+  //   debugger;
+  //   for (let row = 0; row < 9; row++) {
+  //     for (let col = 0; col < 9; col++) {
+  //       if (this._board[row][col] == 0) {
+  //         for (let x = 1; x < 10; x++) {
+  //           if (this.checkRow(row, x)) {
+  //             if(this.checkColumn(col,x)){
+  //               if(this.checkSquare(row,col,x)){
+  //                 this._board[row][col] = x;
+  //                 break;
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return this._board;
+  // }
+  solve(){
+    let tempArray = this.checkEmptyPosition();
+    let i=0;
+    while(i<tempArray.length){
+      let condition = false;
+      let guess = this.board[tempArray[i][0]][tempArray[i][1]];
+      while (!condition && guess < 10) {
+        if (this.checkRow(tempArray[i][0], guess)) {
+          if(this.checkColumn(tempArray[i][1],guess)){
+            if(this.checkSquare(tempArray[i][0],tempArray[i][1],guess)){
+              this.board[tempArray[i][0]][tempArray[i][1]] = guess;
+              condition = true;
+              i++;
+              break;
+            }
+          }
+        }
+        guess++;
+      }
+      if(!condition){
+        this.board[tempArray[i][0]][tempArray[i][1]]=0;
+        i--;
+      }
+    }
+    return this.board;
+  }
 }
 
-// The file has newlines at the end of each line,
-// so we call split to remove it (\n)
-var fs = require('fs')
-var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
-  .toString()
-  .split("\n")[0]
 
-var game = new Sudoku(board_string)
+let game = new Sudoku(board_string)
 
-// Remember: this will just fill out what it can and not "guess"
-game.solve()
 
-console.log(game.board())
+console.log(game.board);
+//console.log(game.checkEmptyPosition());
+//game.solve();
+console.log(game.solve());
